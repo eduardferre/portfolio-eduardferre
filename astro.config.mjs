5a5c7f4f4dea42166b23d5652f52a3b485988be4
eduardferre <eduardferresanchez@gmail.com>
2024-08-19 00:44:09 +0200

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://eduardferre.dev/'
+  site: 'https://eduardferre.dev/',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 })
